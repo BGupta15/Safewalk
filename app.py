3e8232831a7445f155bf01e5d99de5ac66cadf0a
Brinda <shivibrinda000@gmail.com>
2025-06-21 03:12:19 +0530

    dcsb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from twilio.rest import Client
-import json
+import os
 
 app = Flask(__name__)
 
@@ -8,14 +8,7 @@ app = Flask(__name__)
 account_sid = "AC91c4df2979105fcf14ce5a446a12d31f"
 auth_token = "ea09368bf232259933e94ea3038046c0"
 twilio_number = "+12792057151"
-client = Client(account_sid, auth_token)
-
-def send_sms(to_number, message):
-    client.messages.create(
-        body=message,
-        from_=twilio_number,
-        to=to_number
-    )
+emergency_contact = '+916395526762'
 
 @app.route('/')
 def home():
@@ -23,20 +16,13 @@ def home():
 
 @app.route('/sos', methods=['POST'])
 def sos():
-    data = request.json
-    user = data['user']
-    lat = data['location']['lat']
-    lng = data['location']['lng']
-    
-    with open('contacts.json') as f:
-        contacts = json.load(f)
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="🚨 SOS Alert from SafeWalk!\nThis person needs help immediately!",
+        from_=twilio_phone,
+        to=emergency_contact
+    )
+    return "SOS alert sent! You'll receive a message shortly."
 
-    message = f"🚨 SOS Alert for {user}! Location: https://maps.google.com/?q={lat},{lng}"
-    for c in contacts:
-        send_sms(c['phone'], message)
-
-    return jsonify({'status': 'SOS sent'})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81, debug=True)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
